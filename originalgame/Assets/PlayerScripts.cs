c0d8a0542da31cb44aa84f858781e0aa9a81c542
LD10-HIROKI-HOS\Owner <hiroki2525and@icloud.com>
2018-05-03 01:01:14 +0900

    second commit

---
+++ originalgame/Assets/PlayerScripts.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour 
+{
+	public GameObject ball;
+	public float power;
+
+	Rigidbody rbball;
+	Vector3 ballVec;
+
+	Vector3 clickstart, clickend;
+
+	// Use this for initialization
+	void Start () {
+		rbball = rbball.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			clickstart = Input.mousePosition;
+			Debug.Log ("clickstart(" + clickstart);
+		}
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			clickend = Input.mousePosition;
+			Debug.Log ("clickend(" + clickend);
+		}
+		if(clickstart == clickend)
+		{
+			return;
+		}
+		ballVec =(clickend - clickstart);
+		ballVec.z = ballVec.y;
+		ballVec.y = 10;
+		ballVec.Normalize();
+
+		rbball.AddForce (ballVec * power);
+		
+	}
+}
